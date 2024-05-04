2f53d25c6aba97bf16c66c290a7504845cbecbde
enyerjesus06 <167217890+enyerjesus06@users.noreply.github.com>
2024-05-04 12:20:11 -0400

    Revert "Clases para el profesor C#"

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿
+
+//Calculareverything calcular= new Calculareverything();
+
+//calcular.CalcularSueldo();
+
+
+Calculandobase Sumaproducto= new Calculandobase();
+
+Sumaproducto.Calculandosumaproducto();
+
+//Profesor la clase Calculareverything es un codigo al que le agregue mas cosas.
+//pero todo fue con lo aprendido en las dos ultimas clases, echele un ojo espero y llene sus expectativas.
+
+
+
